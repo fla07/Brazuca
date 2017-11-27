02b6cc95be22d14d18e11589992f698ac8fb4d06
Flavia Nunes Pino <flavitta007@gmail.com>
2017-11-27 17:13:06 +0000

    Configurando DB

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ jeito brasileiro de ser, com muita ousadia, alegria, memes e hue - tudo a
 preço de banana.
 
 Todos os arquivos referentes ao trabalho estão na pasta "projeto".
+
+Existe uma tentativa de criar a interface no ELM, mas caso não dê, um modelo foi 
+criado usando HTML e CSS normal, então, vai ter que ser esse mesmo em um plano B
+
+
+Database Credentials
+
+Get credentials for manual connections to this database.
+
+Cancel
+Please note that these credentials are not permanent.
+Heroku rotates credentials periodically and updates applications where this database is attached.
+
+Host
+ec2-174-129-33-159.compute-1.amazonaws.com
+Database
+d31724vubtd5nt
+User
+ybnqinufpyvytj
+Port
+5432
+Password
+fcfb39f0aa850ade8306b144c635e936f049e5b0f4fd50a0c345bae8119036aa
+URI
+postgres://ybnqinufpyvytj:fcfb39f0aa850ade8306b144c635e936f049e5b0f4fd50a0c345bae8119036aa@ec2-174-129-33-159.compute-1.amazonaws.com:5432/d31724vubtd5ntHeroku CLI
+heroku pg:psql postgresql-dimensional-40849 --app haskellfrontend
--- projeto/src/Handler/Home.hs
+++ projeto/src/Handler/Home.hs
@@ -1,73 +1,21 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
-module Handler.Home where
+-- | Common handler functions.
+module Handler.Principal where
 
 import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Text.Julius (RawJS (..))
+import Database.Persist.Postgresql
 
--- Define our data that will be used for creating the form.
-data FileForm = FileForm
-    { fileInfo :: FileInfo
-    , fileDescription :: Text
-    }
-
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
-getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe FileForm
-        handlerName = "getHomeR" :: Text
-    allComments <- runDB $ getAllComments
-
-    defaultLayout $ do
-        let (commentFormId, commentTextareaId, commentListId) = commentIds
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
-
-postHomeR :: Handler Html
-postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
-    allComments <- runDB $ getAllComments
-
-    defaultLayout $ do
-        let (commentFormId, commentTextareaId, commentListId) = commentIds
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
-
-sampleForm :: Form FileForm
-sampleForm = renderBootstrap3 BootstrapBasicForm $ FileForm
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField textSettings Nothing
-    -- Add attributes like the placeholder and CSS classes.
-    where textSettings = FieldSettings
-            { fsLabel = "What's on the file?"
-            , fsTooltip = Nothing
-            , fsId = Nothing
-            , fsName = Nothing
-            , fsAttrs =
-                [ ("class", "form-control")
-                , ("placeholder", "File description")
-                ]
-            }
-
-commentIds :: (Text, Text, Text)
-commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
-
-getAllComments :: DB [Entity Comment]
-getAllComments = selectList [] [Asc CommentId]
+getHomeR = do 
+    defaultLayout $ do 
+        [whamlet|
+            <h1> BEM-VINDO AO SISTEMA DE PRODUTOS
+            <ul>
+                <li> <a href=@{ProdutoR}> Cadastro de Produto
+                <li> <a href=@{ListaProdutoR}> Listagem
+        |]
