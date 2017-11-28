bc3c9eb4affbaeae7130b7674360e066d2dce8d2
Flavia Nunes Pino <flavitta007@gmail.com>
2017-11-28 20:35:43 +0000

    Exemplos de Aula - Modificando

---
+++ projeto/elm-stuff/packages/elm-lang/html/2.0.0/src/Index.elm
@@ -0,0 +1,18 @@
+node "!DOCTYPE" [ attribute "html" "" ]
+    [ node "html" [ lang "pt-br" ]
+        [ node "head" []
+            [ node "meta" [ charset "utf-8" ]
+                []
+            , node "meta" [ content "IE=edge", attribute "http-equiv" "X-UA-Compatible" ]
+                []
+            , node "meta" [ content "width=device-width, initial-scale=1", name "viewport" ]
+                []
+            , node "title" []
+                [ text "Haskell Brazuca" ]
+            , node "link" [ href "css/bootstrap.css", rel "stylesheet" ]
+                []
+            , text ""
+            , text ""
+            ]
+        ]
+    ]
