bbf752138a520828ea061e5e0221f55bb83b0453
brunotl92 <bruno.t.lopes92@gmail.com>
2017-12-04 18:40:30 +0000

    brunotl92 - Criacao tabela Categoria

---
+++ projeto/src/Handler/Categoria.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+-- | Common handler functions.
+module Handler.Categoria where
+
+import Import
+import Database.Persist.Postgresql
